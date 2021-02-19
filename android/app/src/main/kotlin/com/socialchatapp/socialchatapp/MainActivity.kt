4650a3384c4e9d7d7f22b1c9a6017e8a5b5bfc69
Palak Tyagi <tyagipalak121@gmail.com>
2021-02-19 22:34:51 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/socialchatapp/socialchatapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.socialchatapp.socialchatapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:socialchatapp/helper/authenticate.dart';
+import 'package:socialchatapp/helper/helperfunctions.dart';
+import 'package:socialchatapp/views/chatrooms.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool userIsLoggedIn;
+
+  @override
+  void initState() {
+    getLoggedInState();
+    super.initState();
+  }
+
+  getLoggedInState() async {
+    await HelperFunctions.getUserLoggedInSharedPreference().then((value) {
+      setState(() {
+        userIsLoggedIn = value;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutterChat',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xff6200ea),
+        scaffoldBackgroundColor: Color(0xffffc107),
+        accentColor: Color(0xffd500f9),
+        fontFamily: "OverpassRegular",
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: userIsLoggedIn != null
+          ? userIsLoggedIn
+              ? ChatRoom()
+              : Authenticate()
+          : Container(
+              child: Center(
+                child: Authenticate(),
+              ),
+            ),
+    );
+  }
+}
